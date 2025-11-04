941ecc59ae79e51d697eb393e35a5b7b31e85db2
Krish Vora <krishlalitvora@gmail.com>
2025-11-04 08:06:58 +0530

    Added new practice programs

--- Section 8 - Advanced OOPS/Polymorphism/src/Main.java
+++ Section 8 - Advanced OOPS/Polymorphism/src/Main.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 //        Movie theMovie = new Adventure("Star Wars");
 //        theMovie.watchMovie();
 
-        Movie theMovie = Movie.getMovie("Adventure", "Star Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Adventure", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("Enter Type(A for Adventure, C for Comedy, " +
+                    "S for Science Fiction, or Q to quit): ");
+            String type = sc.nextLine();
+            if("Qq".contains(type))
+                break;
+
+            System.out.print("Enter Movie title: ");
+            String title = sc.nextLine();
+
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
     }
 }

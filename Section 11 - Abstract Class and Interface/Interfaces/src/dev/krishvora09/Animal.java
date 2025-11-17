356b8a0697fed8ac787cef6d6d7919f2f085e677
Krish Vora <krishlalitvora@gmail.com>
2025-11-17 09:55:06 +0530

    Added new practice programs

--- Section 11 - Abstract Class and Interface/Interfaces/src/dev/krishvora09/Animal.java
+++ Section 11 - Abstract Class and Interface/Interfaces/src/dev/krishvora09/Animal.java
@@ -1,6 +1,101 @@
 package dev.krishvora09;
 
-interface FlightEnabled{
+enum FlightStages implements Trackable {
+
+    GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+
+        if (this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+
+    public FlightStages getNextStage() {
+
+        FlightStages[] allStages = values();
+        return allStages[(ordinal() + 1) % allStages.length];
+    }
+};
+
+record dragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName() + "is taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + "is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + "is flying");
+    }
+}
+
+class Satellite implements OrbitEarth {
+
+    FlightStages stage = FlightStages.GROUNDED;
+
+    @Override
+    public void achieveOrbit() {
+        transition("Orbit achieved!");
+    }
+
+    @Override
+    public void takeOff() {
+        transition("Taking Off");
+    }
+
+    @Override
+    public void land() {
+        transition("Landing");
+    }
+
+    @Override
+    public void fly() {
+        achieveOrbit();
+        transition("Data Collection while Orbiting");
+    }
+
+    public void transition(String description) {
+
+        System.out.println(description);
+        stage = transition(stage);
+        stage.track();
+    }
+}
+
+interface OrbitEarth extends FlightEnabled {
+
+    void achieveOrbit();
+
+    private static void log(String description) {
+
+        var today = new java.util.Date();
+        System.out.println(today + ": " + description);
+    }
+
+    private void logStage(FlightStages stage, String description) {
+
+        description = stage + ": " + description;
+        log(description);
+    }
+
+    @Override
+    default FlightStages transition(FlightStages stage) {
+
+        FlightStages nextStage = FlightEnabled.super.transition(stage);
+        logStage(stage, "Beginning Transition to " + nextStage);
+        return nextStage;
+    }
+}
+
+interface FlightEnabled {
 
     double MILES_TO_KM = 1.60934;
     double KM_TO_MILES = 0.621371;
@@ -9,9 +104,15 @@ interface FlightEnabled{
     void land();
     void fly();
 
+    default FlightStages transition(FlightStages stage) {
+
+        FlightStages nextStage = stage.getNextStage();
+        System.out.println("Transitioning from " + stage + " to " + nextStage);
+        return nextStage;
+    }
 };
 
-interface Trackable{
+interface Trackable {
 
     void track();
 };

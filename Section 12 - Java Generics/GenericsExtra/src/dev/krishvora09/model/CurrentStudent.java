4dbd074ecb8a74f078fb7ac23519cb2a9e80c683
Krish Vora <krishlalitvora@gmail.com>
2025-11-19 09:52:45 +0530

    Added new practice programs

---
+++ Section 12 - Java Generics/GenericsExtra/src/dev/krishvora09/model/CurrentStudent.java
@@ -0,0 +1,19 @@
+package dev.krishvora09.model;
+
+public class CurrentStudent extends Student{
+
+    private double percentComplete;
+
+    public CurrentStudent() {
+        percentComplete = random.nextDouble(0, 100.001);
+    }
+
+    @Override
+    public String toString() {
+        return "%s %8.1f%%".formatted(super.toString(), percentComplete);
+    }
+
+    public double getPercentComplete() {
+        return percentComplete;
+    }
+}

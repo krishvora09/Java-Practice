45e575408c301df3c27394949da3d994b94224f0
Krish Vora <krishlalitvora@gmail.com>
2025-12-02 09:27:31 +0530

    Added new practice programs

--- Section 15 - Java Collections/SetsAndMaps/src/dev/krishvora09/MapMain.java
+++ Section 15 - Java Collections/SetsAndMaps/src/dev/krishvora09/MapMain.java
@@ -36,12 +36,127 @@ public class MapMain {
 
         System.out.println("----------------------------------------------------");
         contacts.clear();
-        for (Contact contact: fullList) {
+        for (Contact contact : fullList) {
             Contact duplicate = contacts.put(contact.getName(), contact);
             if (duplicate != null) {
-                System.out.println("duplicate = " + duplicate);
-                System.out.println("current = " + contact);
+//                System.out.println("duplicate = " + duplicate);
+//                System.out.println("current = " + contact);
+                contacts.put(contact.getName(), contact.mergeContactData(duplicate));
             }
         }
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        contacts.clear();
+
+        for (Contact contact : fullList) {
+            contacts.putIfAbsent(contact.getName(), contact);
+        }
+
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        contacts.clear();
+
+        for (Contact contact : fullList) {
+            Contact duplicate = contacts.putIfAbsent(contact.getName(), contact);
+            if (duplicate != null) {
+                contacts.put(contact.getName(), contact.mergeContactData(duplicate));
+            }
+        }
+
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        contacts.clear();
+
+//        fullList.forEach(contact -> contacts.merge(contact.getName(), contact,
+//                (previous, current) -> {
+//                    System.out.println("prev: " + previous + " curr: " + current);
+//                    Contact merged = previous.mergeContactData(current);
+//                    System.out.println("merged: " + merged);
+//                    return merged;
+//                }
+//                ));
+
+        fullList.forEach(contact -> contacts.merge(contact.getName(), contact,
+                (previous, current) -> previous.mergeContactData(current)
+        ));
+
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+//        System.out.println("----------------------------------------------------");
+//        for (String contactName : new String[] {"Daisy Duck", "Daffy Duck", "Scrooge McDuck"}) {
+//            contacts.compute(contactName, (k, v) -> new Contact(k));
+//        }
+//        contacts.forEach((k, v) ->
+//                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        for (String contactName : new String[] {"Daisy Duck", "Daffy Duck", "Scrooge McDuck"}) {
+            contacts.computeIfAbsent(contactName, k -> new Contact(k));
+        }
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        for (String contactName : new String[] {"Daisy Duck", "Daffy Duck", "Scrooge McDuck"}) {
+            contacts.computeIfPresent(contactName, (k, v) -> {
+                v.addEmail("Fun Place");
+                return v;
+            });
+        }
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        contacts.replaceAll((k, v) -> {
+            String newEmail = k.replaceAll(" ", "") + "@funplace.com";
+            v.replaceEmailIfExists("DDuck@funplace.com", newEmail);
+            return v;
+        });
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        Contact daisy = new Contact("Daisy Jane Duck", "daisyj@duck.com");
+
+        Contact replacedContact = contacts.replace("Daisy Duck", daisy);
+        System.out.println("daisy = " + daisy);
+        System.out.println("replacedContact = " + replacedContact);
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        Contact updatedDaisy = replacedContact.mergeContactData(daisy);
+        System.out.println("updatedDaisy = " + updatedDaisy);
+//        boolean success = contacts.replace("Daisy Duck", replacedContact, updatedDaisy);
+        boolean success = contacts.replace("Daisy Duck", daisy, updatedDaisy);
+
+        if (success) {
+            System.out.println("Successfully replaced element");
+        } else {
+            System.out.printf("Did not match on both key: %s and value: %s %n"
+                    .formatted("Daisy Duck", replacedContact));
+        }
+
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
+
+        System.out.println("----------------------------------------------------");
+        success = contacts.remove("Daisy Duck", daisy);
+
+        if (success) {
+            System.out.println("Successfully removed element");
+        } else {
+            System.out.printf("Did not match on both key: %s and value: %s %n"
+                    .formatted("Daisy Duck", daisy));
+        }
+
+        contacts.forEach((k, v) ->
+                System.out.println("key= " + k + ", value= " + v));
     }
 }
---
+++ Section 15 - Java Collections/SetsAndMaps/src/dev/krishvora09/MapViewsMain.java
@@ -0,0 +1,34 @@
+package dev.krishvora09;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MapViewsMain {
+
+    public static void main(String[] args) {
+
+        Map<String, Contact> contacts = new HashMap<>();
+        ContactData.getData("phone").forEach(c -> contacts.put(c.getName(), c));
+        ContactData.getData("email").forEach(c -> contacts.put(c.getName(), c));
+
+        Set<String> keysView = contacts.keySet();
+        System.out.println(keysView);
+
+        Set<String> copyOfKeys = new TreeSet<>(contacts.keySet());
+        System.out.println(copyOfKeys);
+
+        if (contacts.containsKey("Linus Van Pelt")) {
+            System.out.println("Linus and I go way back, so of course I have info");
+        }
+
+        keysView.remove("Daffy Duck");
+        System.out.println(keysView);
+        contacts.forEach((k, v) -> System.out.println(v));
+
+        copyOfKeys.remove("Linus Van Pelt");
+        System.out.println(copyOfKeys);
+        contacts.forEach((k, v) -> System.out.println(v));
+    }
+}

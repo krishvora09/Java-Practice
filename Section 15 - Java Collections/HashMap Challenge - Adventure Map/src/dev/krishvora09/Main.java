acb4773f77efa6f99723b47019d56cd0d20ba0ba
Krish Vora <krishlalitvora@gmail.com>
2026-01-05 10:20:22 +0530

    Added new practice programs

---
+++ Section 15 - Java Collections/HashMap Challenge - Adventure Map/src/dev/krishvora09/Main.java
@@ -0,0 +1,27 @@
+package dev.krishvora09;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myLocations = """
+                lake, at the edge of Lake Krish, E: ocean, W: forest, S: well house, N: cave
+                ocean, on Krish's beach before an angry sea, W: lake
+                cave, at the mouth of Krish's bat cave, E: ocean, W: forest, S: lake
+                """;
+
+        AdventureGame game = new AdventureGame(myLocations);
+        game.play("lake");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            String direction = scanner.nextLine().trim().toUpperCase().substring(0, 1);
+            if (direction.equals("Q")) break;
+            game.move(direction);
+        }
+    }
+}

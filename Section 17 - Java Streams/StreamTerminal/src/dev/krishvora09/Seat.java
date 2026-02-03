2a9d8766d78195014c8cc2484f608c4e3e9c2fa7
Krish Vora <krishlalitvora@gmail.com>
2026-02-03 09:27:06 +0530

    Added new practice programs

---
+++ Section 17 - Java Streams/StreamTerminal/src/dev/krishvora09/Seat.java
@@ -0,0 +1,10 @@
+package dev.krishvora09;
+
+public record Seat(char rowMarker, int seatNumber, boolean isReserved) {
+
+    public Seat(char rowMarker, int seatNumber) {
+//        this(rowMarker, seatNumber, new Random().nextBoolean());
+//        this(rowMarker, seatNumber, true);
+        this(rowMarker, seatNumber, false);
+    }
+}

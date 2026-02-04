8f40f6f6062ba3a195ae60c584f45792f9bbaa3e
Krish Vora <krishlalitvora@gmail.com>
2026-02-04 09:32:03 +0530

    Added new practice programs

--- Section 17 - Java Streams/StreamingStudents/src/dev/krishvora09/Main.java
+++ Section 17 - Java Streams/StreamingStudents/src/dev/krishvora09/Main.java
@@ -1,8 +1,23 @@
 package dev.krishvora09;
 
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Course pymc = new Course("PYMC", "Python Masterclass");
+        Course jmc = new Course("JMC", "Java Masterclass");
+//        Student john = new Student("AU", 2019, 27,
+//                "M", true, pymc, jmc);
+//        System.out.println(john);
+//
+//        john.watchLecture("JMC", 10, 5, 2019);
+//        john.watchLecture("PYMC", 7, 7, 2020);
+//        System.out.println(john);
+
+        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
